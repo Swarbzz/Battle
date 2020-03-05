e65496b8708c816b94c52369ca2811194811809b
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-03-05 15:40:30 +0000

    added method so both players take damage

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -7,4 +7,11 @@ feature "seeing health" do
     sign_in_and_play
     expect(page). to have_content 'Barry: 100HP'
   end 
+  scenario " reduce Player 1 HP by 10" do
+    sign_in_and_play
+    attack_and_ok
+    attack_and_ok
+    expect(page).not_to have_content 'George: 100HP'
+    expect(page).to have_content 'George: 90HP'
+  end
 end
